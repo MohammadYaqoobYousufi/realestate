631d04cfc919019feeb8fe11cd4c60e3a0eac11c
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2024-11-20 16:23:01 +0500

    Sign In , Sign Up and forgot password page create

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 export default function Header() {
     const location = useLocation();
     const navigate = useNavigate();
+    
     function pathMathRoute(Route){
      if(Route === location.pathname){
         return true
@@ -20,7 +21,7 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") &&"text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") &&"text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") &&"text-black border-b-red-500"}`}onClick={()=>navigate("/sign-in")}>Sign In</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") &&"text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign In</li>
                 </ul>
             </div>
         </header>
